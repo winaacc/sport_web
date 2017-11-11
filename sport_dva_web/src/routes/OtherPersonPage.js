ff5398d7890418331888d186549da538fb613271
haimeng <910680459@qq.com>
2017-11-11 23:06:55 +0800

    扫一扫查看他人信息逻辑

--- sport_dva_web/src/routes/OtherPersonPage.js
+++ sport_dva_web/src/routes/OtherPersonPage.js
@@ -7,6 +7,7 @@ import {
     WhiteSpace,
     Toast
 } from 'antd-mobile'
+import request from "../utils/request"
 
 export default class App extends React.Component{
     static navConfig = {
@@ -14,18 +15,40 @@ export default class App extends React.Component{
     }
     constructor(props){
         super(props);
+        this.state = {
+            headImage:""
+        }
     }
 
     componentDidMount(){
         var params = this.props.navigation.params;
         Toast.info(JSON.stringify(params));
+        var arr = params.data.split(':');
+        if(arr[0] == 'faceid'){
+            var face_token = arr[1];
+            //获得该用户基本信息
+            this.getUserInfo(face_token);
+        }
+    }
+
+    getUserInfo = async (face_token) => {
+        var result = await request("/getFaceInfo",{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({face_token:face_token})
+        })
+        console.log(result);
+        var url = result.data.image_url;
+        this.setState({headImage:url})
     }
 
     render(){
         return (
             <div>
                 <Flex justify="center" style={{height:200,backgroundColor:'#00ffcc'}}>
-                    <img src="" style={{height:100,width:100,border:'3px solid #fff',borderRadius:'50%'}} />
+                    <img src={this.state.headImage} style={{height:100,width:100,border:'3px solid #fff',borderRadius:'50%'}} />
                 </Flex>
                 <List>
                     <List.Item arrow="horizontal">基本资料</List.Item>
